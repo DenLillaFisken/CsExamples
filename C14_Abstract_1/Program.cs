26c4eb7c9e7ba41c2287d20c99acb056edadd08e
Alexandra Johansson <alexandra_johansson111@live.se>
2020-09-03 14:29:27 +0200

    C14

---
+++ C14_Abstract_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using C14_Abstract_1.Models;
+using System;
+using System.Collections.Generic;
+
+namespace C14_Abstract_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var classlist = new List<Person>(){
+                new Teacher { Firstname = "Hans", LastName = "Johansson", Role = "Teacher" };
+                new Student { FirstName = "Anna", LastName = "Bengtsson" };
+                new Student { FirstName = "Johan", LastName = "Björk" };
+            };
+
+            foreach(var person in classlist)
+            {
+                if(person is Teacher teacher)
+                Console.WriteLine($"{teacher.FirstName} {teacher.LastName} {teacher.Role}");
+                else
+                    Console.WriteLine($"{person.FirstName} {person.LastName}");
+            }
+
+        }
+    }
+}
